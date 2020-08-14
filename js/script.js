3f3cedd4c6a604dc9e1f8857c97ff4626cb84aa7
Ira <irarad1994@gmail.com>
2020-08-14 22:27:16 +0300

    edited buttons

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-function generateBlocks(){
+function generateBlocks() {
   document.getElementById("container").style.display = "flex";
-  
-};
-
+  let color = document.querySelectorAll(".square");
+  for (let i = 0; i < color.length; i++) {
+    color[i].style.backgroundColor = `${getGenerateSquareColor()}`;
+  }
+}
 
 function getGenerateSquareColor() {
-  
-  return '#' + Math.floor(Math.random()*16777215).toString(16)
-}
-function squareColor(){
-  let color =  document.querySelectorAll('.square'); 
-  for (let i = 0; i < color.length; i++) {
-      color[i].style.backgroundColor = `${getGenerateSquareColor()}`;    
-  }
-  
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-const interval = setInterval(() => squareColor(), 1000)
-
+function getInterval() {
+  setInterval(() => generateBlocks(), 1000);
+}
